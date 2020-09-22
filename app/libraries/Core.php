ef6b90d8073aa51b2b58eb2a041a16918141315b
VChouliak <chouliak@hotmail.com>
2020-09-22 19:19:36 +0200

    Create and Implement Core.php

---
+++ app/libraries/Core.php
@@ -0,0 +1,34 @@
+<?php
+
+class Core
+{
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
+
+    public function __construct()
+    {
+        $this->setCurrentController();
+    }
+
+    private function setCurrentController(){
+        $url = $this->getUrl();
+        if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+            $this->currentController = ucwords($url[0]);
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/' .  $this->currentController . '.php';
+        $this->currentController = new $this->currentController;
+    }
+
+    private function getUrl()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
+}

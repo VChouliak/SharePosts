dcf74760c0484ea704c8c9e91f0a3c29be548b6f
VChouliak <chouliak@hotmail.com>
2020-09-22 20:24:27 +0200

    Implemented setMethodAndParamsFromUrl

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -5,21 +5,39 @@ class Core
     protected $currentController = 'Pages';
     protected $currentMethod = 'index';
     protected $params = [];
+    protected $url = "";
 
     public function __construct()
     {
+        $this->url = $this->getUrl();
+
         $this->setCurrentControllerFromUrl();
+        $this->setMethodAndPAramsFromUrl();
+
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
-    private function setCurrentControllerFromUrl(){
-        $url = $this->getUrl();
-        if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
-            $this->currentController = ucwords($url[0]);
-            unset($url[0]);
+    private function setCurrentControllerFromUrl()
+    {
+        if (file_exists('../app/controllers/' . ucwords($this->url[0]) . '.php')) {
+            $this->currentController = ucwords($this->url[0]);
+            unset($this->url[0]);
         }
 
         require_once '../app/controllers/' .  $this->currentController . '.php';
         $this->currentController = new $this->currentController;
+    }
+
+    private function setMethodAndParamsFromUrl()
+    {
+        if (isset($this->url[1]) && isset($this->url[1])) {
+            if (method_exists($this->currentController, $this->url[1])) {
+                $this->currentMethod = $this->url[1];
+
+                unset($this->url[1]);
+            }
+            $this->params = $this->url ? array_values($this->url) : [];
+        }
     }
 
     private function getUrl()

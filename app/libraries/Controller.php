443a64f6395bc4cb671efba12c0f9318c30035a7
VChouliak <chouliak@hotmail.com>
2020-09-23 09:51:36 +0200

    Implement base controller

---
+++ app/libraries/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+class Controller {
+
+    public function loadModel($model){
+        require_once '../app/models' . $model . '.php';
+        return new $model();
+    }
+
+    public function loadView($view, $data = []){
+        if(file_exists('../app/views/' . $view . '.php')){
+            require_once '../app/views/' . $view . '.php';
+        }
+        else{
+            die('View dooes not exists');
+        }
+    }
+}

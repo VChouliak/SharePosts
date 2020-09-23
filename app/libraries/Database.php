078941d683c81af67b1fb823517f347b13309e17
VChouliak <chouliak@hotmail.com>
2020-09-23 14:57:07 +0200

    Implement  methods: execute, getResultSet, getSingleResult, getRowCount

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -26,4 +26,51 @@ class Database
             echo $this->error;
         }
     }
+
+    public function query($sql)
+    {
+        $this->statement = $this->dbHandler->prepare($sql);
+    }
+
+    public function bind($param, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch ($type) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+                    break;
+            }
+        }
+
+        $this->statement->bindValue($param, $value, $type);
+    }
+
+
+    public function execute()
+    {
+        return $this->statement->execute();
+    }
+
+    public function getResultSet(){
+        $this->execute();
+        return $this->statement->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function getSingleResult(){
+        $this->execute();
+        return $this->statement->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function getRowCount(){
+        return $this->statement->rowCount();
+    }
 }

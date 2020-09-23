52682a80f00207f5646ecb6da77420a80a362ba5
VChouliak <chouliak@hotmail.com>
2020-09-23 14:04:53 +0200

    Add db configuration

--- app/config/config.php
+++ app/config/config.php
@@ -3,3 +3,9 @@
 define('APPROOT', dirname(dirname(__FILE__)));
 define('URLROOT', 'http://localhost/SharePosts');
 define('SITENAME', 'SharePosts');
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_USER', 'admin');
+define('DB_PASS', 'admin');
+define('DB_NAME', 'shareposts');

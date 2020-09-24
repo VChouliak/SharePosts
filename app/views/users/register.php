4a36dbcfa7eff766909b2950d665600afbaddf9e
VChouliak <chouliak@hotmail.com>
2020-09-24 11:51:51 +0200

    Update

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -20,7 +20,7 @@
                 <span class="invalid-feedback"><?php echo $data['password_err']?></span>
             </div>
             <div class="form-group">
-                <label for="confirm_password">Password: <sup>*</sup></label>
+                <label for="confirm_password">Confirm Password: <sup>*</sup></label>
                 <input type="password" name="confirm_password" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : '';?>" value="<?php echo $data['confirm_password']?>">
                 <span class="invalid-feedback"><?php echo $data['confirm_password_err']?></span>
             </div>

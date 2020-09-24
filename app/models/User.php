cc583aa705d161d753dd1c9ffe38e04adc8b2c74
VChouliak <chouliak@hotmail.com>
2020-09-24 16:45:25 +0200

    Create and implement User - model

---
+++ app/models/User.php
@@ -0,0 +1,50 @@
+<?php
+class User
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function registerUser($data)
+    {
+        $this->db->query('INSERT INTO users (name, email, password) VALUES(:name, :email, :password)');
+
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':password', $data['password']);
+
+        return $this->db->execute() ? true : false;        
+    }
+
+    public function loginUser($email, $password)
+    {
+        $this->db->query('SELECT * FROM users WHERE email = :email');
+        $this->db->bind(':email', $email);
+
+        $row = $this->db->getSingleResult();
+
+        $hashed_password = $row->password;
+        if (password_verify($password, $hashed_password)) {
+            return $row;
+        } else {
+            return false;
+        }
+    }
+
+
+    public function findUserByEmail($email)
+    {
+        $this->db->query('SELECT * FROM users WHERE email = :email');
+        $this->db->bind(':email', $email);
+        $row = $this->db->getSingleResult();
+
+        if ($this->db->getRowCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

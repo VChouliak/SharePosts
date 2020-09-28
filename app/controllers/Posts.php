e8d73cc99e7482a89199fcd6f449ce89faad8bc1
VChouliak <chouliak@hotmail.com>
2020-09-28 14:45:02 +0200

    Implement "add" method

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,14 +1,16 @@
 <?php
-class Posts extends Controller {
+class Posts extends Controller
+{
 
     public function __construct()
     {
-        if(!isLoggedIn()){
+        if (!isLoggedIn()) {
             redirect('users/login');
         }
         $this->postModel = $this->loadModel('Post');
     }
-    public function index(){
+    public function index()
+    {
         $posts = $this->postModel->getPosts();
         $data = [
             'posts' => $posts
@@ -17,5 +19,51 @@ class Posts extends Controller {
         $this->loadView("posts/index", $data);
     }
 
-    
+    public function add()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'user_id' => $_SESSION['user_id'],
+                'title_err' => '',
+                'body_err' => ''
+            ];
+
+            if (empty($data['title'])) {
+                $data['title_err'] = 'Please enter title';
+            }
+
+            if (empty($data['body'])) {
+                $data['body_err'] = 'Please enter message';
+            }
+
+            if (empty($data['title_err']) && empty($data['body_err'])) {
+                if ($this->postModel->addPost($data)) {
+                    flash('post_message', 'Post Added');
+                    redirect('posts');
+                } else {
+                    die('Error');
+                }
+            } else {
+                $this->loadView('posts/add', $data);
+            }
+        } else {
+            $data = [
+                'title' => '',
+                'body' => ''
+            ];
+        }
+
+
+        $this->loadView("posts/add", $data);
+    }
+
+    public function show($id)
+    {
+        $data = [];
+        $this->loadView('posts/show', $data);
+    }
 }
